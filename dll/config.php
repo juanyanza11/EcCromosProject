7d61308d113e6ddf0e52e790f0628230a92942fd
juanyanza11 <ww4fjj@gmail.com>
2020-11-26 10:13:39 -0500

    Estructura Aplicación

---
+++ dll/config.php
@@ -0,0 +1,7 @@
+<?php
+$site_url="http://127.0.0.1/aplicaciones/ingenieriaWeb2/app2/";
+$local_path="C:\\xampp\htdocs\aplicaciones\ingenieriaWeb2\app2";
+define('DBHOST', '127.0.0.1');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'app1db');
